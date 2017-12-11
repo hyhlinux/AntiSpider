74043208503f7a2bbc15a5612e37b27c7748d58b
huoyinghui <huoyinghui@apkpure.com>
2017-12-11 14:43:54 +0800

    url: aioip/aioauto

--- urls.py
+++ urls.py
@@ -5,6 +5,8 @@ ip_api_urls = [
     # (r'^/api/ips?', VerifyCode.ImageCodeHandler),
     (r'^/api/ip?', Ip.IpHandler),
     (r'^/api/auto', Auto.AutoHandler),
+    (r'^/api/aioip', Ip.AioIpHandler),
+    (r'^/api/aioauto', Auto.AioAutoHandler),
 ]
 
 urls = []
